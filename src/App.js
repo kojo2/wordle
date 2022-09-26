d3bbd6c99e2304b3fc3ed398542256259a86b472
kojo2 <m.hutfield@hotmail.com>
2022-09-26 17:09:03 +0100

    add random word button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import AppWrapper from './AppWrapper';
 import { possibleWords } from './allowedWords';
 import dayjs from 'dayjs';
 import dayOfYear from 'dayjs/plugin/dayOfYear';
+import { useEffect, useState } from 'react';
 
 function App() {
   // get the word selection based on the day of the year
+  const [word, setWord] = useState('');
   dayjs.extend(dayOfYear);
-  const day = dayjs().dayOfYear();
-  const num = Math.round(day * (possibleWords.length / 86400) * 235);
-  const word = possibleWords[num];
+
+  useEffect(() => {
+    const day = dayjs().dayOfYear();
+    const num = Math.round(day * (possibleWords.length / 86400) * 235);
+    setWord(possibleWords[num]);
+  }, []);
+
+  // let word = possibleWords[num];
   return (
     <div className="App">
-      <AppWrapper word={word} />
+      <AppWrapper
+        word={word}
+        setRandomWord={() => {
+          setWord(possibleWords[Math.round(Math.random() * possibleWords.length - 1)]);
+        }}
+      />
     </div>
   );
 }
